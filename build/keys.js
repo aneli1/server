92aaad631f9b13e95d3055d961c0fae67df7d8d3
aneli1 <148299361+aneli1@users.noreply.github.com>
2024-02-12 03:03:33 -0600

    Update keys.js

--- build/keys.js
+++ build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'consultorio'
-    }
+host: 'ls-3e0cc6d0d027fdd2cfbc09a148a4ec0c40bde812.cfsoci82q3h4.us-east-2.rds.ama>
+user: 'dbmasteruser',
+password: 'aguacate',
+database: 'consultorio'
+}
 };
